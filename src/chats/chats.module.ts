9cd9dbbfd6a19c05d49d6e0eaa54ac0c2e5be1fc
joonyeopyoon <yoon.jy@hanwha.com>
2023-10-31 10:55:18 +0900

    feat: model setting

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,20 @@
+import {
+  SocketSchema,
+  Socket as SocketModel,
+  Chatting,
+  ChattingSchema,
+} from './models';
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Chatting.name, schema: ChattingSchema },
+      { name: SocketModel.name, schema: SocketSchema },
+    ]),
+  ],
   providers: [ChatsGateway],
 })
 export class ChatsModule {}
---
+++ src/chats/models/index.ts
@@ -0,0 +1,2 @@
+export * from './chattings.model';
+export * from './sockets.model';
